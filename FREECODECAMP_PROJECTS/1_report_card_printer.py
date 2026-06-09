541443bee6e2ebf904a053d3905503c2487003ae
Gerome Rodriguez <cleestudios@gmail.com>
2026-06-09 17:40:49 +0800

    new outputs

--- FREECODECAMP_PROJECTS/1_report_card_printer.py
+++ FREECODECAMP_PROJECTS/1_report_card_printer.py
@@ -1,12 +1,12 @@
-name = 'Alice'
-print(name, type(name))
+name = 'Alice' # variable name is assigned a string value 'Alice'
+print(name, type(name)) #  print the value of name and its data type
 
-is_student = True
-print(is_student, type(is_student))
+is_student = True # variable is_student is assigned a boolean value True
+print(is_student, type(is_student)) # print the value of is_student and its data type
 
-age = 20
-print(age, type(age))
+age = 20 # variable age is assigned an integer value 20
+print(age, type(age)) # print the value of age and its data type
 
 score = 80.5
-print(isinstance(score, float))
-print(score, type(score))
+print(isinstance(score, float)) # check if score is an instance of the float data type and print the result
+print(score, type(score)) # print the value of score and its data type
